5934e69002176a35b3a63faed21a76685c1bf28d
Shalom Achikasim <shalomachims@gmail.com>
2026-07-13 14:08:18 +0100

    feat: add shared library with generic interface

---
+++ e-commerce.sharedlibrary/Responses/Response.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_commerce.sharedlibrary.Responses
+{
+    public record Response(bool Flag = false, string Message = null!);
+}
