83fb912bc647f4896f3d0c80233c92a30eab46c6
Shalom Achikasim <shalomachims@gmail.com>
2026-07-15 15:14:47 +0100

    fix: rebuild infra layer and fix project paths

---
+++ e-commerce.ProductApiSolution/ProductApi.Application/DTOs/Conversions/ProductConversion.cs
@@ -0,0 +1,45 @@
+﻿using ProductApi.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApi.Application.DTOs.Conversions
+{
+    public static class ProductConversion
+    {
+        public static Product ToEntity(ProductDTO product) => new()
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Quantity = product.Quantity,
+            Price = product.Price
+        };
+
+        public static (ProductDTO?, IEnumerable<ProductDTO>?) FromEntity(Product product, IEnumerable<Product>? products)
+        {
+            if(product is null || products is null)
+            {
+                var singleProduct = new ProductDTO(
+                    product!.Id,
+                    product.Name!,
+                    product.Quantity,
+                    product.Price
+                    );
+
+                return (singleProduct, null);
+            }
+
+            if(product is not null || product is null)
+            {
+                var _products = products.Select(P =>
+                    new ProductDTO(P.Id, P.Name, P.Quantity, P.Price )).ToList();
+
+                return (null, _products);
+            }
+
+            return(null, null);
+        }
+    }
+}
