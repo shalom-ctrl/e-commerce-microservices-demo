8a45e0f1f5beb857a8b6f3cf2ae43acac126f466
Shalom Achikasim <shalomachims@gmail.com>
2026-07-15 09:10:12 +0100

    feat: add global exception handling middleware

---
+++ e-commerce.sharedlibrary/Middleware/GlobalException.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace e_commerce.sharedlibrary.Middleware
+{
+    public class GlobalException(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string message = "Internal Server error occured";
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+            string title = "Error";
+
+            try
+            {
+                await next(context);
+
+                if(context.Response.StatusCode == StatusCodes.Status429TooManyRequests)
+                {
+                    title = "Warning";
+                    message = 
+                }
+            }
+        }
+    }
+}
