05fd3406ce2cb127a28adcfe128839fd3a3b6cf8
Shalom Achikasim <shalomachims@gmail.com>
2026-07-20 12:30:36 +0100

    feat: add order entity, dtos & mapping

---
+++ e-commerce.OrderApiSolution/OrderApi.Application/DTOs/Extensions/OrderConversion.cs
@@ -0,0 +1,49 @@
+﻿using OrderApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApi.Application.DTOs.Extensions
+{
+    public static class OrderConversion
+    {
+        public static Order ToEntity(OrderDTO order) => new()
+        {
+            Id = order.Id,
+            ClientId = order.ClientId,
+            ProductId = order.ProductId,
+            OrderedDate = order.OrderedDate,
+            PurchaseQuantity = order.PurchaseQuantity,
+        };
+
+        public static (OrderDTO?, IEnumerable<OrderDTO>?) FromEntity(Order? order, IEnumerable<Order>? orders)
+        {
+            if(order is not null || orders is null)
+            {
+                var singleOrder = new OrderDTO(order!.Id,
+                    order.ClientId,
+                    order.ProductId, 
+                    order.PurchaseQuantity,
+                    order.OrderedDate);
+
+                return (singleOrder, null);
+            }
+
+           if(order is not null || orders is null)
+            {
+                var _orders = orders!.Select(o =>
+                new OrderDTO(o.Id,
+                o.ClientId,
+                o.ProductId,
+                o.PurchaseQuantity,
+                o.OrderedDate));
+
+                return (null, _orders);
+            }
+
+            return (null, null);
+        }
+    }
+}
