8985426530a1f391957a8b34110a3bfc12d649c7
Shalom Achikasim <shalomachims@gmail.com>
2026-07-20 15:55:28 +0100

    feat: configure OrderApi DI and Polly retry policy

---
+++ e-commerce.OrderApiSolution/OrderApi.Application/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,49 @@
+﻿using e_commerce.sharedlibrary.Logs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderApi.Application.Services;
+using Polly;
+using Polly.Retry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApi.Application.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHttpClient<IOrderService, OrderService>(options =>
+            {
+                options.BaseAddress = new Uri(configuration["ApiGateway:GaseAddress"]!);
+                options.Timeout = TimeSpan.FromSeconds(1);
+            });
+
+            var retryStrategy = new RetryStrategyOptions()
+            {
+                ShouldHandle = new PredicateBuilder().Handle<TaskCanceledException>(),
+                BackoffType = DelayBackoffType.Constant,
+                UseJitter = true,
+                MaxRetryAttempts = 3,
+                Delay = TimeSpan.FromMilliseconds(500),
+                OnRetry = args =>
+                {
+                    string message = $"OnRetry, Attempt: {args.AttemptNumber} Outcome {args.Outcome}";
+                    LogException.LogToConsole(message);
+                    LogException.LogToDebugger(message);
+                    return ValueTask.CompletedTask;
+                }
+            };
+
+            services.AddResiliencePipeline("my-retry-pipeline", builder =>
+            {
+                builder.AddRetry(retryStrategy);
+            });
+
+            return services;
+        }
+    } 
+}
