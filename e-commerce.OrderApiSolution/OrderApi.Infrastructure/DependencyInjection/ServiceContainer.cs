e07e4b36d3602a1a85ecfcc8359383dd5e2c0c2a
Shalom Achikasim <shalomachims@gmail.com>
2026-07-22 11:30:06 +0100

    feat: setup OrderApi infrastructure DI & middleware

---
+++ e-commerce.OrderApiSolution/OrderApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,34 @@
+﻿using e_commerce.sharedlibrary.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderApi.Application.Interfaces;
+using OrderApi.Infrastructure.Data;
+using OrderApi.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration configuration)
+        {
+            SharedServiceContainer.AddSharedServices<OrderDbContext>(services, configuration, configuration["MySerilog:FileName"]!);
+
+            services.AddScoped<IOrder, OrderRepository>();
+
+            return services;
+        } 
+
+        public static IApplicationBuilder UserInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            SharedServiceContainer.UseSharedPolicies(app);
+
+            return app;
+        }
+    }
+}
