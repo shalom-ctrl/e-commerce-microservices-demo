56d2e14dc1bf66a879aae816f21bc0710cc17832
Shalom Achikasim <shalomachims@gmail.com>
2026-07-27 15:24:14 +0100

    refactor: reorganize order solution structure

---
+++ eCommerce.Order/OrderApi.Application/Interfaces/IOrder.cs
@@ -0,0 +1,16 @@
+﻿using e_commerce.sharedlibrary.Interface;
+using OrderApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApi.Application.Interfaces
+{
+    public interface IOrder : IGenericInterface<Order>
+    {
+        Task<IEnumerable<Order>> GetAllOrdersAsync(Expression<Func<Order, bool>> predicate);
+    }
+}
---
+++ eCommerce.Order/OrderApi.Application/Interfaces/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using OrderApi.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApi.Application.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderDTO>> GetOrdersByClientId(int clientId);
+        Task<OrderDetailsDTO> GetOrderDetails(int orderId);
+    }
+}
