fed3f1f5d5132dba574b9366cf8c5746346b7bfa
Shalom Achikasim <shalomachims@gmail.com>
2026-07-27 15:25:06 +0100

    refactor: reorganize product solution structure

---
+++ eCommerce.Product/ProductApi.Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using e_commerce.sharedlibrary.Interface;
+using ProductApi.Domain.Entity;
+
+namespace ProductApi.Application.Interfaces
+{
+    public interface IProductRepository :IGenericInterface<Product>
+    {
+    }
+}
